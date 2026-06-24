f0adc03087447478640c3c7d1ccc905bbe947244
Valen Rionald <vrionald@gmail.com>
2026-06-24 11:52:09 +0700

    Ref: Bottom Nav Fixed

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -6,12 +6,11 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-slate-50 flex justify-center font-sans text-slate-800">
-      <div className="w-full max-w-md bg-white shadow-xl min-h-screen relative flex flex-col">
-        <main className="flex-1 overflow-y-auto pb-20 scrollbar-hide">
+    <div className="h-dvh bg-slate-50 flex justify-center font-sans text-slate-800 overflow-hidden">
+      <div className="w-full max-w-md bg-white shadow-xl h-full relative flex flex-col">
+        <main className="flex-1 overflow-y-auto pb-24 scrollbar-hide">
           {children}
         </main>
-
         <BottomNav />
       </div>
     </div>
